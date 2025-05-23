6cc7f8fef283707ad5f3d846f1553a5820a2917c
Osaze Oviawe <ocubed88@gmail.com>
2025-05-23 23:47:53 +0100

    Add Navbar component and integrate into layout

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const navItems = [
+	{ label: "Home", href: "/" },
+	{ label: "Companions", href: "/companions" },
+	{ label: "My Journey", href: "/my-journey" },
+];
+
+const NavItems = () => {
+	const pathname = usePathname();
+
+	return (
+		<div className="flex items-center gap-4">
+			{navItems.map((nav, idx) => {
+				const { label, href } = nav;
+				const isActive = pathname === href;
+				return (
+					<Link href={href} key={idx} className={cn(isActive && "text-primary font-semibold")}>
+						{label}
+					</Link>
+				);
+			})}
+		</div>
+	);
+};
+
+export default NavItems;
